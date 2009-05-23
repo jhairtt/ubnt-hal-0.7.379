33369fe35aa363c965d696ef7ef962320a714954
Jhair Tocancipa Triana <jtocancipa@dell-desktop.example.com>
2009-05-23 20:56:22 +0100

    Changes.

--- cross_build.sh
+++ cross_build.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
+
 make CC=/home/jtocancipa/local/src/kamikaze_8.09/staging_dir/toolchain-armeb_gcc4.1.2/bin/armeb-linux-gcc TARGET=xscale-be-elf KERNELPATH=/home/jtocancipa/local/src/kamikaze_8.09/build_dir/linux-ixp4xx_generic/linux-2.6.26.6 LD=/home/jtocancipa/local/src/kamikaze_8.09/staging_dir/toolchain-armeb_gcc4.1.2/bin/armeb-linux-ld AR=/home/jtocancipa/local/src/kamikaze_8.09/staging_dir/toolchain-armeb_gcc4.1.2/bin/armeb-linux-ar STRIP=/home/jtocancipa/local/src/kamikaze_8.09/staging_dir/toolchain-armeb_gcc4.1.2/bin/armeb-linux-strip NM=/home/jtocancipa/local/src/kamikaze_8.09/staging_dir/toolchain-armeb_gcc4.1.2/bin/armeb-linux-nm OBJCOPY=/home/jtocancipa/local/src/kamikaze_8.09/staging_dir/toolchain-armeb_gcc4.1.2/bin/armeb-linux-objcopy V=99
